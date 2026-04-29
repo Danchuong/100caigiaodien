6aded4c7cff38cc0abf274706fb06176c47cde37
Zhang Shan <46948684+Danchuong@users.noreply.github.com>
2026-04-29 22:29:43 +0700

    feat: redesign des-1 featured home

--- delivery/des-1-home/home.php
+++ delivery/des-1-home/home.php
@@ -5,30 +5,57 @@
 get_header();
 
 $fallback_img = get_stylesheet_directory_uri() . '/images/no_img.jpg';
-$hero_img     = has_post_thumbnail( get_the_ID() ) ? wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) ) : $fallback_img;
+$hero_img     = get_stylesheet_directory_uri() . '/images/des-1-featured-background.png';
 
-$home_description  = function_exists( 'get_field' ) ? get_field( 'description' ) : '';
-$latest_blog_url   = function_exists( 'get_field' ) ? get_field( 'latest_blog' ) : '#';
-$latest_review_url = function_exists( 'get_field' ) ? get_field( 'latest_review' ) : '#';
-$popular_games_url = function_exists( 'get_field' ) ? get_field( 'popular_games' ) : '#';
+$blog_archive_url   = home_url( '/blogs/' );
+$review_archive_url = home_url( '/reviews/' );
+$games_archive_url  = home_url( '/html5-games/' );
 
-$home_description  = $home_description ? $home_description : get_bloginfo( 'description' );
-$latest_blog_url   = $latest_blog_url ? $latest_blog_url : '#';
-$latest_review_url = $latest_review_url ? $latest_review_url : '#';
-$popular_games_url = $popular_games_url ? $popular_games_url : '#';
+$latest_blog_url   = function_exists( 'get_field' ) ? get_field( 'latest_blog' ) : '';
+$latest_review_url = function_exists( 'get_field' ) ? get_field( 'latest_review' ) : '';
+$popular_games_url = function_exists( 'get_field' ) ? get_field( 'popular_games' ) : '';
+
+$latest_blog_url   = $latest_blog_url ? $latest_blog_url : $blog_archive_url;
+$latest_review_url = $latest_review_url ? $latest_review_url : $review_archive_url;
+$popular_games_url = $popular_games_url ? $popular_games_url : $games_archive_url;
+
+$featured_query = new WP_Query(
+    array(
+        'post_type'      => array( 'blog', 'review', 'post' ),
+        'posts_per_page' => 1,
+        'orderby'        => 'rand',
+        'post_status'    => 'publish',
+    )
+);
+
+$featured_post       = ! empty( $featured_query->posts ) ? $featured_query->posts[0] : null;
+$featured_post_id    = $featured_post ? $featured_post->ID : 0;
+$featured_post_url   = $featured_post ? get_permalink( $featured_post_id ) : '';
+$featured_post_title = $featured_post ? get_the_title( $featured_post_id ) : '';
+$featured_tags       = $featured_post ? get_the_tags( $featured_post_id ) : array();
+$featured_post_type  = $featured_post ? get_post_type( $featured_post_id ) : '';
+$featured_label      = ! empty( $featured_tags ) ? $featured_tags[0]->name : '';
+
+if ( ! $featured_label ) {
+    $featured_label_map = array(
+        'blog'   => 'Blog',
+        'review' => 'Review',
+        'post'   => 'Game',
+    );
+    $featured_label     = isset( $featured_label_map[ $featured_post_type ] ) ? $featured_label_map[ $featured_post_type ] : '';
+}
 ?>
 <section class="home-hero" style="background-image: url(<?php echo esc_url( $hero_img ); ?>)">
     <div class="container">
         <div class="home-hero-wrapper">
             <div class="home-hero-content">
-                <div class="home-hero-label">Game Newsroom</div>
-                <h1><?php echo esc_html( $home_description ); ?></h1>
-                <a href="<?php echo esc_url( $latest_blog_url ); ?>" class="home-hero-button">EXPLORE</a>
-            </div>
-            <div class="home-hero-meta" aria-label="Homepage content groups">
-                <span>Blogs</span>
-                <span>Reviews</span>
-                <span>Games</span>
+                <?php if ( $featured_post ) : ?>
+                    <?php if ( $featured_label ) : ?>
+                        <div class="home-hero-label"><?php echo esc_html( $featured_label ); ?></div>
+                    <?php endif; ?>
+                    <h1><?php echo esc_html( $featured_post_title ); ?></h1>
+                    <a href="<?php echo esc_url( $featured_post_url ); ?>" class="home-hero-button">View feature</a>
+                <?php endif; ?>
             </div>
         </div>
     </div>
@@ -49,6 +76,7 @@ $popular_games_url = $popular_games_url ? $popular_games_url : '#';
                 array(
                     'post_type'      => 'blog',
                     'posts_per_page' => 3,
+                    'post__not_in'   => $featured_post ? array( $featured_post_id ) : array(),
                     'orderby'        => 'rand',
                     'order'          => 'DESC',
                     'post_status'    => 'publish',
@@ -72,7 +100,6 @@ $popular_games_url = $popular_games_url ? $popular_games_url : '#';
                             <span class="home-card-category"><?php echo esc_html( $tag_name ); ?></span>
                             <div class="home-card-content">
                                 <h3><?php echo esc_html( get_the_title() ); ?></h3>
-                                <div class="home-card-desc"><?php echo esc_html( wp_trim_words( get_the_excerpt(), 24 ) ); ?></div>
                             </div>
                         </a>
                     <?php endwhile; ?>
